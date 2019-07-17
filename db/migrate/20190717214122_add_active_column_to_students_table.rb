cf0274585bb1580a27f9e7f732f0992c67c05349
Anthony Rurup <tcrurup@gmail.com>
2019-07-17 21:52:23 +0000

    Automatically backed up by Learn

--- db/migrate/20190717214122_add_active_column_to_students_table.rb
+++ db/migrate/20190717214122_add_active_column_to_students_table.rb
@@ -1,5 +1,5 @@
 class AddActiveColumnToStudentsTable < ActiveRecord::Migration[5.0]
   def change
-    add_column :students, :active, :boolean, :default => true
+    add_column :students, :active, :boolean, :default => false
   end
 end
