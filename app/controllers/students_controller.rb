da7d81da43bd3c7d04b015460a4ceb73909fed48
Anthony Rurup <tcrurup@gmail.com>
2019-07-17 22:10:34 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
-    @student.active? ? @student.active = false : @student.active = true
+    @student.active ? @student.active = false : @student.active = true
     @student.save
   end
 
