62ad3f6972d1aeb43ebef950767b4969c695508f
Anthony Rurup <tcrurup@gmail.com>
2019-07-17 21:55:11 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+
   end
 
   private
